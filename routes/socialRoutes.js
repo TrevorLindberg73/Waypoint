c85149e32129800fec63ae2f9e22be28799b8b6f
Trevor Lindberg <t01lindberg@gmail.com>
2023-11-09 16:53:56 -0500

    Touch up on socialRoutes

--- routes/socialRoutes.js
+++ routes/socialRoutes.js
@@ -4,17 +4,25 @@ const {fileUpload} = require ('../middleware/fileUpload');
 
 const router = express.Router();
 
+//GET /socialmedia: send all events to user
 router.get('/', controller.index);
 
-
-
-//New Post
+//GET /socialmedia/new: send html form for creating new post
 router.get('/new', controller.create);
 
-//Detailed Post
+//GET /socialmedia: creates new post
+router.post('/', controller.create);
+
+//GET /socialmedia/:id: send post to user by ID
 router.get('/:id', controller.showDetailed);
 
-//Delete Post
+//GET /socialmedia/:id/edit: send html form for editing
+router.get('/:id/edit', controller.edit);
+
+//PUT /socialmedia/:id: update post by ID
+router.put('/:id', controller.update);
+
+//DELETE /socialmedia/:id: deletes post by ID
 router.get('/:id', controller.delete)
 
 router.get('/:id/edit', controller.update);
