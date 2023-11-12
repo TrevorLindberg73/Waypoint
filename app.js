ecb7a572ddb10bd045779171721572a05557e98f
Trevor Lindberg <t01lindberg@gmail.com>
2023-11-12 18:52:01 -0500

    Help with user DB

--- app.js
+++ app.js
@@ -37,6 +37,12 @@ app.use('/articles', newsRoutes);
 app.use('/socialmedia', socialRoutes);
 app.use('/lfg', lfgRoutes);
 
+app.use((req, res, next) => {
+    let err = new Error('The server can not find ' + req.url);
+    err.status = 404;
+    next(err);
+});
+
 // app.get('/login', (req,res) =>{
 //     res.render('login');
 // })
--- models/user.js
+++ models/user.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
 const userSchema = new Schema({
     firstName: {type: String, required: [true, 'Cannot be empty']},
     lastName: {type: String, required: [true, 'Cannot be empty']},
