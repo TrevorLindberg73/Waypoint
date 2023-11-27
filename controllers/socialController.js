e3210cf7606dee6cdc0369df599869ed77fcdfc7
Trevor Lindberg <t01lindberg@gmail.com>
2023-11-27 14:28:23 -0500

    Basic Posts functioning

--- controllers/socialController.js
+++ controllers/socialController.js
@@ -6,7 +6,19 @@ exports.index = (req,res) => {
 }
 
 exports.create = (req,res,next) => {
-    
+    let post = new model(req.body);
+    //let user = new User('User', 'User', 'email', 'password');
+
+    //post.user = user;
+    post.save()
+    .then((post)=>res.redirect('/socialmedia'))
+    .catch(err=>{
+        if(err.name === 'ValidationError') {
+            err.status = 400;
+        }
+        next(err);
+    });
+
 }
 
 exports.showNewPost = (req,res,next) =>{
--- models/social.js
+++ models/social.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const { DateTime } = require('luxon');
 
 const socialSchema = new Schema({
     title: {type: String},
     description: {type: String},
-    img: {type: String, unique: true},
-    user: {type: Schema.Types.ObjectId, ref: 'User'},
-});
+    //user: {type: Schema.Types.ObjectId, ref: 'User'},
+    date: {type: String}
+},
+{timestamps: true}
+);
 
 module.exports = mongoose.model('Social', socialSchema);
