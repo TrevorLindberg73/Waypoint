17f7d53df158374c92b4f05fb0d69390a1a98674
Jemina Ubaldo <j3minaubaldo@gmail.com>
2023-12-10 13:17:34 -0500

    SocialMedia and Profile Tab Changes

--- controllers/socialController.js
+++ controllers/socialController.js
@@ -1,13 +1,20 @@
 const model = require ('../models/social');
+const User = require('../models/user'); 
 const {fileUpload} = require('../middleware/fileUpload');
 
-
-
 // Index
+// exports.index = (req,res) => {
+//     model.find().then(posts=>{        
+//         res.render('./social_media/index', {posts})}).catch(err=>next(err));
+// }
+
 exports.index = (req,res) => {
-    model.find().then(posts=>{        
-        res.render('./social_media/index', {posts})}).catch(err=>next(err));
-    
+    let id = req.session.user;
+    Promise.all([User.findById(id), model.find()])
+    .then(([user, posts]) => {
+        res.render('./social_media/index', { user, posts});
+    })
+    .catch(err => next(err));
 }
 // Create
 exports.create = (req,res,next) => {
@@ -44,13 +51,56 @@ exports.showDetailed = (req, res) =>{
         .catch(err=>next(err));
 };
 
-exports.edit = (req,res,next) =>{
+// exports.edit = (req,res,next) =>{
+//     let id = req.params.id;
+
+//     model.findById(id)
+//     .then(post=>{return res.render('./social_media/editform', {post})})
+//     .catch(err=>next(err));
+// }
+
+exports.edit = (req, res, next) => {
     let id = req.params.id;
+    let userId = req.session.user;
 
     model.findById(id)
-    .then(post=>{return res.render('./social_media/editform', {post})})
-    .catch(err=>next(err));
-}
+        .lean()
+        .then(post => {
+            if (post) {
+                // Check if the user is logged in
+                if (userId) {
+                    User.findById(userId)
+                        .then(user => {
+                            res.render('./social_media/editform', { post, user });
+                        })
+                        .catch(err=>next(err));
+                } else {
+                    // If not logged in, render the edit event template without user information
+                    res.render('./social_media/editform', { post });
+                }
+            } else {
+                let err = new Error('Cannot find post with id ' + id);
+                err.status = 404;
+                next(err);
+            }
+        })
+        .catch(err => next(err));
+};
+
+// exports.update = (req,res,next) =>{
+
+//     let post = req.body;
+//     let id = req.params.id;
+
+//     model.findByIdAndUpdate(id, post, {useFindAndModify: false, runValidators: true})
+//     .then(post=>{res.redirect('/social_media/'+id)})
+//     .catch(err=>{
+//         if(err.name === 'ValidationError') {
+//             err.status = 400;
+//         }
+//         next(err)
+//     });
+// }
 
 exports.update = (req,res,next) =>{
 
@@ -58,7 +108,7 @@ exports.update = (req,res,next) =>{
     let id = req.params.id;
 
     model.findByIdAndUpdate(id, post, {useFindAndModify: false, runValidators: true})
-    .then(post=>{res.redirect('/social_media/'+id)})
+    .then(post=>{res.redirect('/user/profile')})
     .catch(err=>{
         if(err.name === 'ValidationError') {
             err.status = 400;
@@ -67,10 +117,18 @@ exports.update = (req,res,next) =>{
     });
 }
 
+// exports.delete = (req, res, next) =>{
+//     let id = req.params.id;
+    
+//     model.findByIdAndDelete(id, {useFindAndModify: false})
+//     .then(post=>{res.redirect('/social_media')})
+//     .catch(err=>next(err));
+// }
+
 exports.delete = (req, res, next) =>{
     let id = req.params.id;
     
     model.findByIdAndDelete(id, {useFindAndModify: false})
-    .then(post=>{res.redirect('/social_media')})
+    .then(post=>{res.redirect('/user/profile')})
     .catch(err=>next(err));
 }
